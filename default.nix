7a5f20a2e464a1303c248388db308f31978153e8
Valentin Reis <vreis@anl.gov>
2020-05-05 16:37:11 -0500

    testing recursive gitlab-ci files.

--- default.nix
+++ default.nix
@@ -1,7 +1,2 @@
-let argotest =
-  builtins.fetchTarball
-  "https://xgitlab.cels.anl.gov/argo/argotest/-/archive/master/argotest-master.tar.gz";
-in import "${argotest}/test.nix" {
-  libnrm-override = ./.;
-  testName = "base";
-}
+{ pkgs ? import (../.) { } }:
+pkgs
