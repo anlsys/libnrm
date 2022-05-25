9514f42fb23c867e1f2f1c5271386059dfb4ebed
Swann Perarnau <swann@anl.gov>
2022-05-25 09:01:53 -0500

    ci fixes

--- nix/libnrm.nix
+++ nix/libnrm.nix
@@ -1,7 +1,7 @@
-{ stdenv, autoreconfHook, pkgconfig, zeromq, czmq, jansson, check, protobufc }:
+{ stdenv, autoreconfHook, pkgconfig, zeromq, czmq, jansson, check, protobufc, git }:
 stdenv.mkDerivation {
   src = ../.;
   name = "libnrm";
-  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig git ];
   buildInputs = [ zeromq check jansson czmq protobufc ];
 }
