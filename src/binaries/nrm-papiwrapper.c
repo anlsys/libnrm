4f64b004d18ef1233c0e45f242a45c2d82199ea4
Swann Perarnau <swann@anl.gov>
2023-07-10 15:53:34 -0500

    ci

--- src/binaries/nrm-papiwrapper.c
+++ src/binaries/nrm-papiwrapper.c
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright 2021 UChicago Argonne, LLC.
+ * Copyright 2019 UChicago Argonne, LLC.
  * (c.f. AUTHORS, LICENSE)
  *
- * This file is part of the nrm-extra project.
- * For more info, see https://github.com/anlsys/nrm-extra
+ * This file is part of the libnrm project.
+ * For more info, see https://github.com/anlsys/libnrm
  *
  * SPDX-License-Identifier: BSD-3-Clause
- *******************************************************************************/
+ ******************************************************************************/
 
 /* Filename: perfwrapper.c
  *
