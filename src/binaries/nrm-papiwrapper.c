525183c780c69eea66edc530b7e66308ce0d973a
Swann Perarnau <swann@anl.gov>
2023-07-10 14:34:27 -0500

    working?

--- src/binaries/nrm-papiwrapper.c
+++ src/binaries/nrm-papiwrapper.c
@@ -17,9 +17,9 @@
 #include "config.h"
 
 #include "nrm.h"
+#include "nrm/tools.h"
 
 #include <errno.h>
-#include <getopt.h>
 #include <math.h>
 #include <papi.h>
 #include <sched.h>
@@ -31,11 +31,6 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
-
-
-#define UPSTREAM_URI "tcp://127.0.0.1"
-#define PUB_PORT 2345
-#define RPC_PORT 3456
 
 int find_allowed_scope(nrm_client_t *client, nrm_scope_t **scope)
 {
@@ -75,117 +70,70 @@ int find_allowed_scope(nrm_client_t *client, nrm_scope_t **scope)
 
 int main(int argc, char **argv)
 {
-	int c, err;
-	double freq = 1;
-	char(*EventCodeStrs)[PAPI_MAX_STR_LEN] = NULL;
-	int EventCodeCnt = 0;
-	int ret = EXIT_FAILURE;
+	int err;
+	nrm_tools_args_t args;
 
+	nrm_init(NULL, NULL);
+	nrm_log_init(stderr, "nrm-papiwrapper");
+
+	int ret = EXIT_FAILURE;
 	nrm_client_t *client;
 	nrm_scope_t *scope;
-	nrm_sensor_t **sensors = NULL;
+	nrm_vector_t *sensors = NULL;
 	long long *counters = NULL;
+	size_t EventCodeCnt = 0;
 
-	int log_level = NRM_LOG_ERROR;
+	args.progname = "nrm-papiwrapper";
+	args.flags = NRM_TOOLS_ARGS_FLAG_FREQ | NRM_TOOLS_ARGS_FLAG_EVENT;
 
-	const char *usage =
-	        "Usage: nrm-perfwrapper [options] [command]\n"
-	        "     options:\n"
-	        "            -e, --event             PAPI preset event name. Default: PAPI_TOT_INS\n"
-	        "            -f, --frequency         Frequency in hz to poll. Default: 10.0\n"
-	        "            -v, --verbose           Produce verbose output. Log messages will be displayed to stderr\n"
-	        "            -h, --help              Displays this help message\n";
-
-	while (1) {
-		static struct option long_options[] = {
-		        {"verbose", no_argument, 0, 'v'},
-		        {"frequency", required_argument, 0, 'f'},
-		        {"help", no_argument, 0, 'h'},
-		        {"event", required_argument, 0, 'e'},
-		        {0, 0, 0, 0}};
-
-		int option_index = 0;
-		c = getopt_long(argc, argv, "+vf:e:h", long_options,
-		                &option_index);
-
-		if (c == -1)
-			break;
-		switch (c) {
-		case 0:
-			break;
-		case 'v':
-			log_level = NRM_LOG_DEBUG;
-			break;
-		case 'f':
-			errno = 0;
-			freq = strtod(optarg, NULL);
-			if (errno != 0 || freq <= 0) {
-				fprintf(stderr,
-				        "Error parsing the frequency\n");
-				goto cleanup_preinit;
-			}
-			break;
-		case 'e':
-			if ((EventCodeStrs = realloc(
-			             EventCodeStrs,
-			             (EventCodeCnt + 1) *
-			                     sizeof(*EventCodeStrs))) == NULL) {
-				fprintf(stderr, "realloc failed\n");
-				goto cleanup_preinit;
-			}
-			strcpy(EventCodeStrs[EventCodeCnt++], optarg);
-			break;
-		case 'h':
-			fprintf(stderr, "%s", usage);
-			goto cleanup_preinit;
-		case '?':
-		default:
-			fprintf(stderr, "Wrong option argument\n");
-			fprintf(stderr, "%s", usage);
-			goto cleanup_preinit;
-		}
+	err = nrm_tools_parse_args(argc, argv, &args);
+	if (err < 0) {
+		nrm_log_error("errors during argument parsing\n");
+		nrm_tools_print_help(&args);
+		exit(EXIT_FAILURE);
 	}
 
+	if (args.ask_help) {
+		nrm_tools_print_help(&args);
+		exit(EXIT_SUCCESS);
+	}
+	if (args.ask_version) {
+		nrm_tools_print_version(&args);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (err >= argc) {
+		nrm_log_error("Expected command after options.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	/* remove the parsed part */
+	argc -= err;
+	argv = &(argv[err]);
+
+	nrm_log_setlevel(args.log_level);
+
+	nrm_vector_length(args.events, &EventCodeCnt);
 	if (EventCodeCnt == 0) {
 		EventCodeCnt = 1;
-		if ((EventCodeStrs = malloc(sizeof(*EventCodeStrs))) == NULL) {
-			fprintf(stderr, "malloc failed\n");
-			goto cleanup_preinit;
-		}
-		strcpy(EventCodeStrs[0], "PAPI_TOT_INS");
+		nrm_string_t def = nrm_string_fromchar("PAPI_TOT_INS");
+		nrm_vector_push_back(args.events, &def);
 	}
-
-	if (nrm_init(NULL, NULL) != 0) {
-		fprintf(stderr, "nrm_init failed\n");
-		goto cleanup_preinit;
-	}
-	if (nrm_log_init(stderr, "nrm.extra.perf") != 0) {
-		fprintf(stderr, "nrm_log_init failed\n");
-		goto cleanup_postinit;
-	}
-
-	nrm_log_setlevel(log_level);
-	nrm_log_debug("NRM logging initialized.\n");
 
 	// create client
-	if (nrm_client_create(&client, UPSTREAM_URI, PUB_PORT, RPC_PORT) != 0 ||
-	    client == NULL) {
+	if (nrm_client_create(&client, args.upstream_uri, args.pub_port,
+			      args.rpc_port) != 0 || client == NULL) {
 		nrm_log_error("Client creation failed\n");
 		goto cleanup_postinit;
 	}
 
 	nrm_log_debug("NRM client initialized.\n");
 
-	nrm_log_debug("verbose=%d; freq=%f\n", log_level, freq);
 	nrm_log_debug("Events:\n");
-	for (int i = 0; i < EventCodeCnt; i++)
-		nrm_log_debug("[%d]=%s\n", i, EventCodeStrs[i]);
-
-	if (optind >= argc) {
-		nrm_log_error("Expected command after options.\n");
-		goto cleanup_client;
+	nrm_vector_foreach(args.events, iterator) {
+		nrm_string_t *s = nrm_vector_iterator_get(iterator);
+		nrm_log_debug("%s\n", *s);
 	}
-
 
 	if (find_allowed_scope(client, &scope) != 0) {
 		nrm_log_error("Finding scope failed\n");
@@ -194,26 +142,28 @@ int main(int argc, char **argv)
 	nrm_log_debug("NRM scope initialized.\n");
 
 	/* create our sensors and add them to the daemon */
-	if ((sensors = calloc(EventCodeCnt, sizeof(*sensors))) == NULL) {
+	nrm_vector_create(&sensors, sizeof(nrm_sensor_t *));
+	if (sensors == NULL) {
 		nrm_log_error("Allocating sensors failed\n");
 		goto cleanup_scope;
 	}
-	for (int i = 0; i < EventCodeCnt; i++) {
-		nrm_string_t sensor_name =
-			nrm_string_fromprintf("nrm.extra.perf.%s.%u",
-					      getpid(), EventCodeStrs[i]);
-		if ((sensors[i] = nrm_sensor_create(sensor_name)) == NULL) {
+	for (size_t i = 0; i < EventCodeCnt; i++) {
+		nrm_string_t *eventName;
+		nrm_string_t sensor_name;
+
+		nrm_vector_get_withtype(nrm_string_t, args.events, i, eventName);
+		sensor_name = nrm_string_fromprintf("nrm.extra.perf.%s.%u", *eventName, getpid());
+		nrm_sensor_t *sensor = nrm_sensor_create(sensor_name);
+		nrm_string_decref(sensor_name);
+		if (sensor == NULL) {
 			nrm_log_error("Sensor creation failed\n");
-			nrm_string_decref(sensor_name);
 			goto cleanup_sensor;
 		}
-		free(sensor_name);
-		if (nrm_client_add_sensor(client, sensors[i]) != 0) {
-			nrm_string_decref(sensor_name);
+		if (nrm_client_add_sensor(client, sensor) != 0) {
 			nrm_log_error("Adding sensor failed\n");
 			goto cleanup_sensor;
 		}
-		nrm_string_decref(sensor_name);
+		nrm_vector_push_back(sensors, &sensor);
 	}
 
 	// initialize PAPI
@@ -242,9 +192,11 @@ int main(int argc, char **argv)
 		              PAPI_strerror(err));
 		goto cleanup;
 	}
-	for (int i = 0; i < EventCodeCnt; i++) {
+	for (size_t i = 0; i < EventCodeCnt; i++) {
 		int EventCode;
-		err = PAPI_event_name_to_code(EventCodeStrs[i], &EventCode);
+		nrm_string_t *EventName;
+		nrm_vector_get_withtype(nrm_string_t, args.events, i, EventName);
+		err = PAPI_event_name_to_code(*EventName, &EventCode);
 		if (err != PAPI_OK) {
 			nrm_log_error("PAPI event_name translation error: %s\n",
 			              PAPI_strerror(err));
@@ -259,12 +211,12 @@ int main(int argc, char **argv)
 
 		nrm_log_debug(
 		        "PAPI code string %s converted to PAPI code %i, and registered.\n",
-		        EventCodeStrs[i], EventCode);
+		        EventName, EventCode);
 	}
 
 	int pid = fork();
 	if (pid < 0) {
-		nrm_log_error("perfwrapper fork error\n");
+		nrm_log_error("fork error\n");
 		goto cleanup;
 	} else if (pid == 0) {
 		/* child, needs to exec the cmd */
@@ -275,7 +227,7 @@ int main(int argc, char **argv)
 				_exit(EXIT_FAILURE);
 			}
 
-		err = execvp(argv[optind], &argv[optind]);
+		err = execvp(argv[0], &argv[0]);
 		nrm_log_error("Error executing command: %s\n", strerror(errno));
 		_exit(EXIT_FAILURE);
 	}
@@ -329,7 +281,7 @@ int main(int argc, char **argv)
 
 	do {
 		/* sleep for a frequency */
-		long long sleeptime = 1e9 / freq;
+		long long sleeptime = 1e9 / args.freq;
 		struct timespec req, rem;
 		req.tv_sec = sleeptime / 1000000000;
 		req.tv_nsec = sleeptime % 1000000000;
@@ -351,8 +303,11 @@ int main(int argc, char **argv)
 		nrm_time_gettime(&time);
 		nrm_log_debug("NRM time obtained.\n");
 
-		for (int i = 0; i < EventCodeCnt; i++) {
-			if (nrm_client_send_event(client, time, sensors[i],
+		for (size_t i = 0; i < EventCodeCnt; i++) {
+			nrm_sensor_t **sensor;
+			nrm_vector_get_withtype(nrm_sensor_t *, sensors,
+						i, sensor);
+			if (nrm_client_send_event(client, time, *sensor,
 			                          scope, counters[i]) != 0) {
 				nrm_log_error(
 				        "Sending event to the daemon error\n");
@@ -375,24 +330,29 @@ int main(int argc, char **argv)
 	/* final send here */
 	PAPI_stop(EventSet, counters);
 	nrm_time_gettime(&time);
-	for (int i = 0; i < EventCodeCnt; i++)
-		nrm_client_send_event(client, time, sensors[i], scope,
+	for (size_t i = 0; i < EventCodeCnt; i++) {
+		nrm_sensor_t **sensor;
+		nrm_vector_get_withtype(nrm_sensor_t *, sensors, i, sensor);
+		nrm_client_send_event(client, time, *sensor, scope,
 		                      counters[i]);
+	}
 
 cleanup:
 	free(counters);
 cleanup_sensor:
-	for (int i = 0; i < EventCodeCnt; i++)
-		if (sensors[i])
-			nrm_sensor_destroy(&sensors[i]);
-	free(sensors);
+	for (size_t i = 0; i < EventCodeCnt; i++) {
+		nrm_sensor_t **sensor;
+		nrm_vector_get_withtype(nrm_sensor_t *, sensors, i, sensor);
+		if (*sensor)
+			nrm_sensor_destroy(sensor);
+	}
+	nrm_vector_destroy(&sensors);
 cleanup_scope:
 	nrm_scope_destroy(scope);
 cleanup_client:
 	nrm_client_destroy(&client);
 cleanup_postinit:
+	nrm_tools_args_destroy(&args);
 	nrm_finalize();
-cleanup_preinit:
-	free(EventCodeStrs);
-	exit(ret);
+	return ret;
 }
