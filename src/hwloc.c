6d7530d75c000fa7df0877c6a4e2d3e154d0c7af
Swann Perarnau <swann@anl.gov>
2023-08-24 13:17:58 -0500

    more

--- src/hwloc.c
+++ src/hwloc.c
@@ -101,42 +101,43 @@ int nrm_scope_hwloc_scopes(nrm_hash_t **scopes)
 	// OS objects don't have cpusets/nodesets, see
 	// https://www.open-mpi.org/projects/hwloc/doc/v2.5.0/a00363.php
 
-	/* assume that the GPUs are balanced, and find the depth of the first
-	 * one.
-	 */
-	int depth_of_gpus = 0;
+	// check first if we're seeing subdevices
+	int subdevices = 0;
 	for (int i = 0; i < num_of_osdev; i++) {
 		hwloc_obj_t object =
 		        hwloc_get_obj_by_type(topology, HWLOC_OBJ_OS_DEVICE, i);
-		assert(object->cpuset == NULL);
-		if (object->attr->osdev.type == HWLOC_OBJ_OSDEV_GPU ||
-		    object->attr->osdev.type == HWLOC_OBJ_OSDEV_COPROC) {
-			depth_of_gpus = object->depth;
-			break;
-		}
-	}
-	if (depth_of_gpus != 0) {
-		for (int i = 0, counter = 0; i < num_of_osdev; i++) {
-			hwloc_obj_t object = hwloc_get_obj_by_type(
-			        topology, HWLOC_OBJ_OS_DEVICE, i);
-			assert(object->cpuset == NULL);
-			if (object->depth != depth_of_gpus)
-				continue;
+		if (object->attr->osdev.type != HWLOC_OBJ_OSDEV_GPU &&
+		    object->attr->osdev.type != HWLOC_OBJ_OSDEV_COPROC)
+			continue;
 
-			if (object->attr->osdev.type == HWLOC_OBJ_OSDEV_GPU ||
-			    object->attr->osdev.type ==
-			            HWLOC_OBJ_OSDEV_COPROC) {
-				nrm_scope_t *this_scope = NULL;
-				snprintf(scope_name, sizeof(scope_name), "%s%s",
-				         namespace, object->name);
-				this_scope = nrm_scope_create(scope_name);
-				nrm_scope_add(this_scope, NRM_SCOPE_TYPE_GPU,
-				              counter);
-				counter++;
-				nrm_hash_add(scopes, nrm_scope_uuid(this_scope),
-				             this_scope);
-			}
-		}
+		if (object->io_arity != 0)
+			subdevices = 1;
+	}
+
+	for (int i = 0, counter = 0; i < num_of_osdev; i++) {
+		hwloc_obj_t object =
+		        hwloc_get_obj_by_type(topology, HWLOC_OBJ_OS_DEVICE, i);
+		assert(object->cpuset == NULL);
+
+		if (nrm_bitmap_isset(&done, object->logical_index))
+			continue;
+
+		/* only looking for GPUs */
+		if (object->attr->osdev.type != HWLOC_OBJ_OSDEV_GPU &&
+		    object->attr->osdev.type != HWLOC_OBJ_OSDEV_COPROC)
+			continue;
+
+		/* if we have subdevices, we don't count them */
+		if (subdevices && object->io_arity == 0)
+			continue;
+
+		nrm_scope_t *this_scope = NULL;
+		snprintf(scope_name, sizeof(scope_name), "%s%s", namespace,
+		         object->name);
+		this_scope = nrm_scope_create(scope_name);
+		nrm_scope_add(this_scope, NRM_SCOPE_TYPE_GPU, counter);
+		counter++;
+		nrm_hash_add(scopes, nrm_scope_uuid(this_scope), this_scope);
 	}
 	hwloc_topology_destroy(topology);
 	return 0;
