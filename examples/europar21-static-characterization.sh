8feb53095c4f2d7ee07233f2baec8b273dc66822
Swann Perarnau <swann@anl.gov>
2023-09-07 13:15:15 -0500

    draft script

---
+++ examples/europar21-static-characterization.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# Script to perform data collection for the static characterization of an
+# application, in line with our Europar21 methodology on modeling the
+# progress/powercap relationship.
+#
+# Eventually we should replace this with a python script using the python
+# bindings, but just using the cli tools for now should be fine.
+
+# make sure we stop on errors, and log everything
+set -e
+set -x
+set -u
+DATE=`date +%Y%m%d.%H%M%S`
+LOGFILE="$DATE.log"
+LOGDIR="$DATE"
+echo "Start: $DATE"
+DELIMITER="################################################################################"
+
+# remember this script in the log
+echo -e "$DELIMITER\n$DELIMITER\n\n" >> $LOGFILE
+cat $0 >> $LOGFILE
+echo -e "\n$DELIMITER\n" >> $LOGFILE
+
+# Gather some info on the system
+uname -a >> $LOGFILE
+cat /proc/cmdline >> $LOGFILE
+lscpu >> $LOGFILE
+
+echo -e "\n$DELIMITER\n" >> $LOGFILE
+
+# save environment
+env >> $LOGFILE
+ulimit -n 4096
+ulimit -a >> $LOGFILE
+
+echo -e "\n$DELIMITER\n" >> $LOGFILE
+
+# CONFIG: prefixes, commands
+############################
+
+PREFIX=$HOME/local
+
+REPEATS=1
+PRELOAD_PATH=$PREFIX/lib/libnrm-ompt.so
+
+export PATH=$PREFIX/bin:$PATH
+
+# Experiment
+############################
+
+mkdir -p $LOGDIR
+nrm-setup -o $LOGDIR &
+NRM_SETUP_PID=$!
+
+# wait for nrm-setup to start sleeping
+while [ ! -e $DATE/nrm-setup-ready.log ]; do
+	sleep 1
+done
+
+# launch sensor/actuator
+nrm-geopm -vvv 1>$LOGDIR/nrm-geopm-stdout.log 2>$LOGDIR/nrm-geopm-stderr.log &
+NRM_GEOPM_PID=$1
+sleep 1
+
+for pcap in `nrmc find-actuator nrm.geopm.cpu.power | jq -r '.[0].choices | join("\n")'`
+do
+	nrmc actuate nrm.geopm.cpu.power $pcap
+	for i in `seq 1 $REPEATS`;
+	do
+		echo "power $pcap $i" >> $LOGFILE
+		nrmc listen >> $LOGDIR/$pcap.$i.log
+		NRM_LISTEN_PID=$!
+		nrmc run -d $PRELOAD_PATH XSBench -x large
+		kill $NRM_LISTEN_PID
+	done
+done
+
+kill $NRM_GEOPM_PID
+kill $NRM_SETUP_PID
