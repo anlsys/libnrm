dd7c50001c6c1387a6f139609f2085c50821f2cf
Swann Perarnau <swann@anl.gov>
2025-06-05 08:59:22 -0500

    [ci] update geopm deps, ubuntu images, nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz") {}
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz") {}
 }:
 let
   callPackage = pkgs.lib.callPackageWith pkgs;
 in pkgs // rec {
-  libnrm = pkgs.callPackage ./nix/libnrm.nix { openmp = pkgs.llvmPackages_15.openmp; };
+  libnrm = pkgs.callPackage ./nix/libnrm.nix { openmp = pkgs.llvmPackages_17.openmp; };
 }
